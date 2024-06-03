3a6f16d901bd9b9ab469dcf7bfd26bb8d50251d2
Dean Ruina <dean.ruina@csc.fi>
2024-06-03 14:59:11 +0300

    changes to wasm

--- swift_browser_ui_frontend/wasm/src/include/streamingdownload.h
+++ swift_browser_ui_frontend/wasm/src/include/streamingdownload.h
@@ -15,14 +15,5 @@ Open crypt4gh header for file decryption and return session key.
 uint8_t *get_session_key_from_header(const KEYPAIR *kp, const char *header);
 
 
-/*
-Decrypt a crypt4gh encrypted chunk using the session key.
-*/
-CHUNK *decrypt_chunk(
-    const uint8_t *session_key,
-    uint8_t *segment,
-    size_t len_segment
-);
-
 
 #endif
