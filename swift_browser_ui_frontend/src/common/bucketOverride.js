839f2d2e17db49c74e1bba482294608a8a631a0b
Dean Ruina <dean.ruina@csc.fi>
2024-06-11 15:19:50 +0300

    Finnish wording fixed

--- swift_browser_ui_frontend/src/common/bucketOverride.js
+++ swift_browser_ui_frontend/src/common/bucketOverride.js
@@ -185,19 +185,206 @@ let bucket_overrides = {
       index: {
         loginmethods: [
           {
-            msg: "Kirjaudu Haka:lla",
+            msg: "Kirjaudu Haka-tunnuksella",
             href: "/login",
           },
           {
-            msg: "Kirjaudu CSC käyttäjällä",
+            msg: "Kirjaudu CSC-tunnuksella",
             href: "/loginpassword",
           },
         ],
       },
+      error: {
+        inUse: "Kontin nimi on jo käytössä.",
+        inUseOtherPrj: "Kontin nimi on jo käytössä toisessa projektissa.",
+        invalidName: "Kontin nimi tai asiasana ei kelpaa.",
+        createFail: "Kontin luonti epäonnistui.",
+        forbiddenChars: "Kontin nimi ei voi sisältää muita " +
+        "erikoismerkkejä kuin piste(.), viiva(-) ja alaviiva(_)",
+        segments: "Kontin nimi ei saa päättyä '_segments'.",
+      },
+      dropFiles: "Vedä ja pudota kontit tähän tai ",
+      createFolder: "Luo Kontti",
       program_name: "Allas Web UI",
       program_description: "Allas Web UI tarjoaa yksinkertaisen " +
           "web-käyttöliittymän CSC:n Object Storage -palveluun, Altaaseen.",
       helplink: "https://docs.csc.fi/data/Allas/",
+      folderTabs: {
+        all: "Kaikki kontit",
+        sharedFrom: "Jakamasi kontit",
+        sharedTo: "Sinulle jaetut kontit",
+      },
+      folderDetails: {
+        notShared: "Tätä konttia ei ole jaettu toiselle projektille.",
+        sharing_to_one_project: "Tämä kontti on jaettu yhdelle projektille.",
+        sharing_to_many_projects: "Tämä kontti on jaettu useille projekteille.",
+        shared_with_view:
+          "Voit tarkastella tätä konttia. (@:message.share.view_perm)",
+        shared_with_read:
+          "Voit kopioida konttisi, ladata " +
+          "objekteja tässä kontissa ja purkaa kontin sisällön " +
+          "salauksen. (@:message.share.read_perm)",
+        shared_with_read_write:
+          "Voit kopioida ja ladata " +
+          "objekteja sekä purkaa kontin sisällön salauksen. " +
+          "Voit lähettää uusia tai poistaa jo kontissa olevia objekteja. " +
+          "(@:message.share.write_perm)",
+      },
+      table: {
+        folderDetails: "Ei yksityiskohtia kontteille",
+        fileDown: "Objektin lataus",
+        item: "Objekti",
+        items: "Objektit",
+        itemsPerPage: "Objektit sivulla: ",
+      },
+      tableOptions: {
+        render: "Näytä objektit konteissa",
+        text: "Näytä objektit polkuina",
+      },
+      share: {
+        share_id_tooltip:
+          "Tällä toiminnolla voit kopioida jakamistunnuksen: uniikin " +
+          "32-numeroisen koodin, joka on yhdistetty {tooltipb} " +
+          "projektiin. Lähetä tunnus (esim. sähköpostilla) muiden projektien " +
+          "jäsenille, niin he voivat jakaa kontteja sinulle.",
+        instructions: "Kuinka jaan kontin",
+        share_title: "Jaa kontti",
+        share_guide_intro: "Kun haluat jakaa kontin toisen projektin kanssa: ",
+        share_guide_step1:
+          "Sinun tulee tietää " +
+          "vastaanottavan projektin jakamistunnus (32-numeroinen " +
+          "koodi). Vastaanottaja voi kopioida " +
+          "jakamistunnuksen Kopioi jakamistunnus -napilla " +
+          "ja lähettää sen sinulle esim. sähköpostilla. " +
+          "Voit jakaa kontin useiden projektien kanssa.",
+        view_perm_desc:
+          ": Vastaanottavan projektin jäsenet voivat tarkastella kontin " +
+          "sisältöä. Käytä tätä, kun tarvitset varmuuden, ettei " +
+          "ojektijasi jaeta eteenpäin. Huomaa, että " +
+          "sinun tulee olla myös vastaanottavan projektin omistaja.",
+        read_perm: "Siirrä objektit",
+        read_perm_desc:
+          ": Vastaanottavan projektin jäsenet voivat kopioida konttisi " +
+          ", ladata objektit sekä purkaa kontin sisällön salauksen. " +
+          "Käytä tätä, kun haluat siirtää objektejasi toiselle projektille.",
+        write_perm_desc:
+          ": @:message.share.read_perm -oikeuksien lisäksi vastaanottavan " +
+          "projektin jäsenet voivat lähettää uusia tai poistaa jo kontissa " +
+          "olevia objekteja. Käytä tätä, kun haluat käyttää " +
+          "konttia jaettuna työtilana.",
+        shared_successfully: "Kontin jakaminen onnistui.",
+        shared_table_title: "Tämä kontti on jaettu",
+        fail_duplicate: "Kontti on jo jaettu kyseiselle projektille.",
+        emptyContainer: "Tämä kontti on tyhjä.",
+      },
+      emptyProject: {
+        all: "Tässä projektissa ei ole kontteja.",
+        sharedFrom: "Et ole jakanut yhtään konttia.",
+        sharedTo: "Sinulle ei ole jaettu kontteja.",
+      },
+      containers: "Kontit - ",
+      download: {
+        files: "Objektit voidaan ladata vain " +
+          "erikseen, koska objektien tai alikontioiden nimet ovat " +
+          "yli 99 merkkiä pitkiä.",
+        gathering: "Haetaan listaa objektitiedostoista",
+        warnTempFiles: "Väliaikaisten objektien tai kontioiden " +
+        "(.crdownload, .crswap) avaaminen voi keskeyttää latauksen.",
+      },
+      upload: {
+        duplicate: "Objektit, joilla on samat polut, eivät ole sallittuja.",
+        sizeZero: "Tyhjiä objekteja ei voi lähettää.",
+        viewDestinationFolder: "Näytä kohdekontti",
+        uploadedItems: "Lähetetyt objektit näytetään pian",
+        addFiles: "Lisää ladattavat objektit.",
+        accessFail: "Konttiin ei ole pääsyä.",
+      },
+      copysuccess: "Konttia kopioidaan",
+      copyfail: "Kontin kopiointi epäonnistui",
+      notDecryptable: "Salauksen purkaminen epäonnistui osassa objekteista.",
+      container_ops: {
+        addContainer: "Luo uusi Kontti",
+        norename:
+          "Konttia ei voi nimetä uudelleen, " +
+          "mutta sen voi kopioida uudella nimellä.",
+        createdFolder: "Luotu kontti jaetaan kaikille jäsenille projektissa ",
+        viewProjectMembers: "Näytä projektin jäsenet",
+        deleteNote:
+          "Kontin poistaminen edellyttää kaikkien " +
+          "objektien poistamista ensin.",
+        deleteSuccess: "Kontti poistettu",
+        folderName: "Kontin nimi",
+      },
+      subfolders: {
+        deleteNote:
+          "Alikontin poistaminen edellyttää sen kaikkien " +
+          "objektien poistamista.",
+        deleteOneSuccess: "Alikontti poistettu.",
+        deleteManySuccess: "Alikontit poistettu.",
+      },
+      objects: {
+        file: "Objekti ",
+        files: "Objektit ",
+        overwriteConfirm:
+          " on jo olemassa. Haluatko korvata objektin? " +
+          "(Edellinen objekti poistetaan.)",
+        overwriteConfirmMany:
+          " ovat jo olemassa. Haluatko korvata Objektit? " +
+          "(Edelliset objektit poistetaan.)",
+        deleteConfirm: "Poista objektit",
+        deleteObjects: "Poista objektit",
+        deleteManySuccess: " objektit poistettu",
+        deleteOneSuccess: " objekti poistettu",
+        deleteSharedObjects:
+         "Tällä toiminnolla poistat " +
+         "objektit jaetusta kontiosta pysyvästi. " +
+         "Haluatko varmasti poistaa nämä objektit?",
+        deleteObjectsMessage:
+          "Objekteja ei voi palauttaa poistamisen jälkeen. " +
+          "Haluatko varmasti poistaa nämä objektit?",
+      },
+      replicate: {
+        copy_folder: "Kopioi kontti: ",
+        name_newFolder: "Nimeä uusi kontti",
+      },
+      encrypt: {
+        uploadFiles: "Lataa objektit",
+        uploadDestination: "Kohdekontti: ",
+        upload_step1: "Luo uusi kontti",
+        upload_step2: "Ladattavat objektit",
+        dropMsg: "Valitse objektit",
+        empty: "Ei valittuja objekteja",
+        uploadedFiles:
+          "Lähetetyt objektit jaetaan kaikille jäsenille projektissa ",
+        uploadedToShared:
+          "ja myös kaikille jäsenille muissa projekteissa, " +
+          "joilla on pääsy tähän jaettuun konttiin.",
+      },
+      search: {
+        container: "Kontti",
+        object: "Objekti",
+        folder: "Alikontti",
+        objects: "Objektit",
+        buildingIndex:
+          "Tässä projektissa on paljon objekteja. Odota " +
+          "hetki ja yritä uudelleen.",
+      },
+      select: {
+        description:
+          "Käyttäjällä on pääsy rajoitettuihin projekteihin. " +
+          "Selatessa rajoitettua projektia käyttöliittymän pääsy on " +
+          "rajattu, eli vain rajatun projektin sisältö on näkyvissä. " +
+          "Objektin kopiointi ja siirto projektista toiseen, ja " +
+          "muiden projektien sisällön selailu on estetty. Valitse " +
+          "projekti, jota haluat käyttää. Valinnan jälkeen projektin " +
+          "vaihto onnistuu vain kirjautumalla ulos. Mikäli haluat " +
+          "selailla vain rajoittamattomia projekteja, paina " +
+          "rajoittamattomien projektien nappia alla.",
+      },
+    },
+    label: {
+      folder_tabs: "erityyppisiä kontteja",
+      searchbox: "etsi kontteja",
     },
   },
 };
