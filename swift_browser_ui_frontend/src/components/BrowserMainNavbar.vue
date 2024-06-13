b79a89f99fc3922962e6e5d937efa06ad33d6143
Dean Ruina <dean.ruina@csc.fi>
2024-06-13 09:21:53 +0300

    icon toggeling fixed

--- swift_browser_ui_frontend/src/components/BrowserMainNavbar.vue
+++ swift_browser_ui_frontend/src/components/BrowserMainNavbar.vue
@@ -41,7 +41,7 @@
             >
               <i
                 class="mdi pr-3 menu-icon"
-                :class="bucketIconClass"
+                :class="iconIndex"
               />
               <span />
             </button>
@@ -120,7 +120,7 @@ export default {
       currentLang: "",
       extLinkIcon: mdiOpenInNew,
       projectInfoLink: "",
-      isBucketIconActive: false,
+      iconIndexnum: 0,
     };
   },
   computed: {
@@ -133,8 +133,9 @@ export default {
     locale () {
       return this.$i18n.locale;
     },
-    bucketIconClass() {
-      return this.isBucketIconActive ? "mdi-bucket" : "mdi-folder";
+    iconIndex () {
+      const icons = ["mdi-folder", "mdi-package", "mdi-bucket"];
+      return icons[this.iconIndexnum];
     },
   },
   watch: {
@@ -250,8 +251,10 @@ export default {
       }, 300);
     },
     toggleBucketOverrides() {
-      this.isBucketIconActive = !this.isBucketIconActive;
-      toggleBucketOverrides();
+      this.iconIndexnum = (this.iconIndexnum + 1) % 3;
+      if (this.iconIndexnum !== 2) {
+        toggleBucketOverrides();
+      }
     },
   },
 };
